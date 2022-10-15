8571e7c735df47368e29d5f89f6f014e78cf282a
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-10-15 12:23:53 -0500

    Setup Async/Await function for API call
    -Since the API call is dependent on user input, I created an async/Await
    function that receives the weatherData json after user inputs the city.
    -I can now destructure this data.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const key = "84d17c513e484ffdba40caf570beb7ef";
+const form = document.getElementById("main__form");
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=${key}`)
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+async function getWeatherData(userInput) {
+  const key = "84d17c513e484ffdba40caf570beb7ef";
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${key}`;
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (e) {
+    console.log("Error:", e.message);
+  }
+}
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault;
+  const userInput = document.querySelector(".main__input").value;
+  const weatherData = await getWeatherData(userInput);
+  console.log(weatherData);
+});
