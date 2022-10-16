97d99f85df37a200325ec6d62f6e188eac3b68e8
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-10-16 13:04:46 -0500

    created container for weather cards and edited URL to imperial units:

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const form = document.getElementById("main__form");
+const container = document.querySelector(".card__container");
 
 //City Input
 async function getWeatherData(userInput) {
   const key = "84d17c513e484ffdba40caf570beb7ef";
   try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${key}&units=imperial`;
     const response = await fetch(url);
     const weatherData = await response.json();
     return weatherData;
@@ -13,21 +14,30 @@ async function getWeatherData(userInput) {
   }
 }
 
-async function renderWeather() {
+async function renderWeatherData() {
   const userInput = document.querySelector(".main__input").value;
   const weatherData = await getWeatherData(userInput);
 
+  //Destructured JSON
   const {
-    main,
+    main: { temperature },
     name,
     sys: { country },
-    weather: { description },
+    weather,
   } = weatherData;
   console.log(weatherData);
+
+  return temperature, name, country, weather;
+}
+
+function creatWeatherCard() {}
+
+function addCardToContainer() {
+  container.appendChild(card);
 }
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault;
-  getWeatherData();
-  renderWeather();
+  renderWeatherData();
+  creatWeatherCard();
 });
